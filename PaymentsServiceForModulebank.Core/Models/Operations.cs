fd7048d19a66c2d5e92494e92e835487ca59eb86
sergxlove <sergxlove@mail.ru>
2026-08-01 23:00:08 +0600

    add operations and outbox model, repository, add endpoints: /health, /operations, /operations/{id}/submit

---
+++ PaymentsServiceForModulebank.Core/Models/Operations.cs
@@ -0,0 +1,30 @@
+﻿namespace PaymentsServiceForModulebank.Core.Models
+{
+    public enum OperationStatus
+    {
+        CREATED,
+        PROCESSING,
+        COMPLETED,
+        REJECTED
+    }
+    public class Operations
+    {
+        public string OperationId { get; set; } = string.Empty;
+
+        public decimal Amount { get; set; }
+
+        public string Currency { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public OperationStatus Status { get; set; }
+
+        public string? ProviderPaymentId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public int RetryCount { get; set; }
+    }
+}
---
+++ PaymentsServiceForModulebank.Core/Models/OutboxMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace PaymentsServiceForModulebank.Core.Models
+{
+    public class OutboxMessages
+    {
+        public Guid Id { get; set; }
+        public string OperationId { get; set; } = string.Empty;
+        public string Payload { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public int RetryCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
